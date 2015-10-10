89e7e6c4047232c2c8ad557242eacf45494c6974
lichinlichin <lichin.it@gmail.com>
2015-10-10 17:30:47 +0800

    the coding world's door
    new page.

---
+++ javascripts/index.js
@@ -0,0 +1,24 @@
+var lichin_index = function() {
+	var lichin_colorbgtimer = null,
+		  cur_index = 1,rand_index;
+	var max = 13,min = 1,flag = false;
+	var colorbgchanging = function() {
+		setInterval(function(){
+			var dif = max - min;
+			flag = false;
+			while(!flag){
+				rand_index = min+Math.round(Math.random() * dif);
+				if(rand_index != cur_index){
+					cur_index = rand_index;
+					flag = true;
+				}
+			}
+			$('body').css('background-image','url(./images/bg/'+cur_index+'.jpg)');
+		},25000);
+	}
+	return {
+		init: function(){
+			colorbgchanging();
+		}
+	}
+}();
